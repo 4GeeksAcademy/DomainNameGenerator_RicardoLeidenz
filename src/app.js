f75bab6e2f7a6f79ce173ec4257575e83a1776a1
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-07-21 23:24:09 +0000

    Fixed a bug in conditionals

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 const DOMAINS = document.getElementById("allDomains");
+let pronoun = ["the", "my", "your"];
+let adj = ["Best", "Good", "Ugly"];
+let noun = ["Windigo", "Monkey", "Cabinet"];
+let extensions = ["net", "key", "go"];
+let domainList = "";
 
-window.onload = function() {
-  let pronoun = ["the","my","your"];
-  let adj = ["Best","Good","Ugly"];
-  let noun = ["Windigo","Monkey","Cabinet"];
-  let extensions = ["net","key","go"];
-  let domainList = "";
+window.onload = function () {
   //We cycle through all pronouns, adjetives, nouns and extensions to print all possible combinations
-  for (let itemPronoun of pronoun){
-    for (let itemAdj of adj){
-      for (let itemNoun of noun){
-        for (let itemExtensions of extensions){
-          if (domainList != ""){
+  for (let itemPronoun of pronoun) {
+    for (let itemAdj of adj) {
+      for (let itemNoun of noun) {
+        for (let itemExtensions of extensions) {
+          if (domainList != "") {
             domainList += " | ";
           }
-          domainList += generateDomain(itemPronoun, itemAdj,itemNoun,itemExtensions);
+          domainList += generateDomain(itemPronoun, itemAdj, itemNoun, itemExtensions);
         }
       }
     }
@@ -22,11 +22,12 @@ window.onload = function() {
   DOMAINS.innerHTML = domainList;
 };
 
-function generateDomain(domainPronun, domainAdj,domainNoun,domainExtension){
-  let wordplay = ""
-  if (domainNoun.slice(-domainExtension.length) == domainExtension){
-   wordplay =  domainPronun + domainAdj + domainNoun.slice(0,domainNoun.length - domainExtension.length) + "." + domainExtension;
+function generateDomain(domainPronun, domainAdj, domainNoun, domainExtension) {
+  let wordplay = "";
+  //Compares the last leters on domainNoun to the domainExtension
+  if (domainNoun.slice(domainNoun.length - domainExtension.length) == domainExtension) {
+    wordplay = domainPronun + domainAdj + domainNoun.slice(0, domainNoun.length - domainExtension.length) + "." + domainExtension;
   }
-  if (wordplay != "") return domainPronun + domainAdj + domainNoun + "." + domainExtension + " | " + wordplay;
+  if (wordplay.length > 0) return domainPronun + domainAdj + domainNoun + "." + domainExtension + " | " + wordplay;
   return domainPronun + domainAdj + domainNoun + "." + domainExtension;
 }
