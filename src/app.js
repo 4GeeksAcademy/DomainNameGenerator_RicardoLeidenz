0ff1e895f521a5e71250a5fabff4111dbd91eda8
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-07-21 21:51:20 +0000

    Cleaned up code and added a way to show domains on screen

--- src/app.js
+++ src/app.js
@@ -1,34 +1,28 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+const myParagraph = document.getElementById("allDomains");
 window.onload = function() {
-  //write your code here
   let pronoun = ["the","my","your"];
   let adj = ["Best","Good","Ugly"];
   let noun = ["Mofongo","Monkey","Cabinet"];
   let extensions = ["net","key","go"];
-  let domain = "";
+  let domains = "";
   //We cycle through all pronouns, adjetives, nouns and extensions to print all possible combinations
   for (let itemPronoun of pronoun){
     for (let itemAdj of adj){
       for (let itemNoun of noun){
         for (let itemExtensions of extensions){
+          if (domains != ""){
+            domains += ", ";
+          }
           //We check if the extension matches the ending of the nouns and if it does we skip it to do the play of words with the extension
           if (itemNoun.slice(-itemExtensions.length) == itemExtensions){
-            domain = itemPronoun + itemAdj + itemNoun.slice(0,itemNoun.length - itemExtensions.length) + "." + itemExtensions;
-            console.log(domain);
+            domains += itemPronoun + itemAdj + itemNoun.slice(0,itemNoun.length - itemExtensions.length) + "." + itemExtensions;
+            console.log(itemPronoun + itemAdj + itemNoun.slice(0,itemNoun.length - itemExtensions.length) + "." + itemExtensions);
           }
-          //Else just print like normal
-          else{
-          domain = itemPronoun + itemAdj + itemNoun + "." + itemExtensions;
-          console.log(domain);
-          }
+          domains += itemPronoun + itemAdj + itemNoun + "." + itemExtensions;
+          console.log(itemPronoun + itemAdj + itemNoun + "." + itemExtensions);
         }
       }
     }
   }
+  myParagraph.innerHTML = domains;
 };
