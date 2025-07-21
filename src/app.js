cc30ab60b3c07bd3c3553b7a84badd88d3ea45c9
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-07-21 21:58:03 +0000

    Fixed some final bugs and code readability

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
-const myParagraph = document.getElementById("allDomains");
+const DOMAINS = document.getElementById("allDomains");
 window.onload = function() {
   let pronoun = ["the","my","your"];
   let adj = ["Best","Good","Ugly"];
   let noun = ["Mofongo","Monkey","Cabinet"];
   let extensions = ["net","key","go"];
-  let domains = "";
+  let domainList = "";
   //We cycle through all pronouns, adjetives, nouns and extensions to print all possible combinations
   for (let itemPronoun of pronoun){
     for (let itemAdj of adj){
       for (let itemNoun of noun){
         for (let itemExtensions of extensions){
-          if (domains != ""){
-            domains += ", ";
+          if (domainList != ""){
+            domainList += " | ";
           }
           //We check if the extension matches the ending of the nouns and if it does we skip it to do the play of words with the extension
           if (itemNoun.slice(-itemExtensions.length) == itemExtensions){
-            domains += itemPronoun + itemAdj + itemNoun.slice(0,itemNoun.length - itemExtensions.length) + "." + itemExtensions;
+            domainList += itemPronoun + itemAdj + itemNoun.slice(0,itemNoun.length - itemExtensions.length) + "." + itemExtensions + " | ";
             console.log(itemPronoun + itemAdj + itemNoun.slice(0,itemNoun.length - itemExtensions.length) + "." + itemExtensions);
           }
-          domains += itemPronoun + itemAdj + itemNoun + "." + itemExtensions;
+          domainList += itemPronoun + itemAdj + itemNoun + "." + itemExtensions;
           console.log(itemPronoun + itemAdj + itemNoun + "." + itemExtensions);
         }
       }
     }
   }
-  myParagraph.innerHTML = domains;
+  DOMAINS.innerHTML = domainList;
 };
